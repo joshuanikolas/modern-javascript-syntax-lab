62304b3420d5a20010e2a10dc4e6f36709f62297
joshuanikolas <nikolassnchez@icloud.com>
2024-11-21 17:28:19 -0600

    exercise 11 complete

--- app.js
+++ app.js
@@ -120,6 +120,43 @@ let statement = pizza === 'tasty' ? 'yum' : 'yuck'
 console.log(statement)
 
 
+// 1. SET LANGUAGE
+
+// Construct a single line of code that assigns a default value using the logical OR operator. This line should match the logic of the following statement: 
+
+// "LANG is equal to localLangConfig or the default value of English."
+
+const localLangConfig = null;  // Change to 'es', 'fr', etc., or keep it null
+
+// a. Create a variable called LANG
+// b. Assign LANG the value of localLangConfig or 'en' as a default
+
+// Your code here
+
+// Log the result
+const LANG = localLangConfig || 'en'
+
+console.log('Language setting:', LANG);
+
+// 2. SET WEBSITE THEME
+
+const userSavedTheme = null; // Change to 'dark', 'contrast', etc., or keep it null
+
+// a. Create a variable called USER_THEME
+// b. Assign USER_THEME the value of userSavedTheme or 'light' as a default
+
+// Your code here
+
+// Log the result
+const USER_THEME = userSavedTheme || 'light'
+
+console.log('User theme setting:', USER_THEME);
+
+
+
+
+
+
 
 
 
